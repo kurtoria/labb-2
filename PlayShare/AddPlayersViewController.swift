fb647dbd40b51322181892a4224cdd58a4ee1484
Victoria Grönqvist <victoriagronqvist@Victorias-MacBook-Air.local>
2018-04-16 11:56:57 +0200

    Made it possible to delete players

--- PlayShare/AddPlayersViewController.swift
+++ PlayShare/AddPlayersViewController.swift
@@ -14,6 +14,7 @@ class AddPlayersViewController: UIViewController, UITableViewDataSource, UITable
     @IBOutlet weak var playersList: UITableView!
     @IBOutlet weak var tableView: UITableView!
     
+    
     //var players : [String] = []
     var players : [String] = []
     
@@ -35,7 +36,6 @@ class AddPlayersViewController: UIViewController, UITableViewDataSource, UITable
         reload(tableView)
     }
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return players.count
     }
@@ -45,11 +45,49 @@ class AddPlayersViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell") as! UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell")!
+        
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
         cell.textLabel?.text = players[indexPath.row]
         
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //players[indexPath.row]
+        print("\(indexPath.row) was clicked and the item was \(players[indexPath.row])")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell")!
+        
+        cell.textLabel?.text = "Bob"
+        players[indexPath.row] = (cell.textLabel?.text!)!
+        reload(tableView)
+
+        
+    }
+    
+    
+    // #warning fix this
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            print("Deleting \(players[indexPath.row])")
+            players.remove(at: indexPath.item)
+            //reload(tableView)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        //print("Using canEditRowAt")
+        return true
+    }
+ 
+ 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
